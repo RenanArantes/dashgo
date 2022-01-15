ec8533adc03e6f844f2754c7928654928ed71acf
RenanArantes <malevo18@gmail.com>
2022-01-15 15:05:50 -0300

    Pagina: Sign in

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,59 @@
+import { Flex, Input, Button, Stack, FormLabel, FormControl } from '@chakra-ui/react'
+
 export default function Home() {
-  return <h1>Next App</h1>
+  return (
+    <Flex
+      width="100vw"
+      height="100vh"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Flex
+        as="form"
+        w="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing="4">
+          <FormControl>
+            <FormLabel htmlFor="email">Email</FormLabel>
+            <Input
+              name="email"
+              type="email"
+              id="email"
+              focusBorderColor="pink.500"
+              backgroundColor="gray.900"
+              variant="filled"
+              _hover={{
+                bgColor: 'gray.900'
+              }}
+              size="lg" />
+          </FormControl>
+          
+          <FormControl>
+            <FormLabel htmlFor="password">Password</FormLabel>
+            <Input
+              name="password"
+              type="password"
+              id="password"
+              focusBorderColor="pink.500"
+              backgroundColor="gray.900"
+              variant="filled"
+              _hover={{
+                bgColor: 'gray.900'
+              }}
+              size="lg" />
+          </FormControl>
+        </Stack>
+        <Button
+          type="submit"
+          marginTop="6"
+          colorScheme="pink"
+          size="lg"> Entrar </Button>
+      </Flex>
+    </Flex>
+  )
 }
