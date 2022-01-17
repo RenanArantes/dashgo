b819e8b322b3e9ba0252fca168cec148b439a870
RenanArantes <malevo18@gmail.com>
2022-01-17 10:35:43 -0300

    Fluxo de navegacao

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -1,15 +1,18 @@
 import { Text } from '@chakra-ui/react'
+import Link from 'next/link'
 
 export default function Logo() {
     return (
-        <Text
-            fontSize={["2xl", "3xl"]}
-            fontWeight="bold"
-            letterSpacing="tight"
-            width="64"
-        >
-            dashgo
-            <Text as="span" marginLeft="1" color="pink.500">.</Text>    
-        </Text>
+        <Link href="/dashboard" passHref>
+            <Text
+                fontSize={["2xl", "3xl"]}
+                fontWeight="bold"
+                letterSpacing="tight"
+                width="64"
+            >
+                dashgo
+                <Text as="span" marginLeft="1" color="pink.500">.</Text>    
+            </Text>
+        </Link>
     )
 }
