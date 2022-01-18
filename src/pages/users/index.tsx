43fd05447b4ebbe73379540e3362e7a5b106c907
RenanArantes <malevo18@gmail.com>
2022-01-18 10:12:18 -0300

    Listagem de usuarios

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -14,7 +14,20 @@ export default function UserList() {
         const response = await fetch('http://localhost:3000/api/users')
         const data = await response.json()
         
-        return data
+        const users = data.users.map(user => {
+            return {
+                id: user.id,
+                name: user.name,
+                email: user.email,
+                createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR',{
+                    day: '2-digit',
+                    month: 'long',
+                    year: 'numeric'
+                })
+            }
+        })
+
+        return users
     })
 
     const isWideVersion = useBreakpointValue({
@@ -49,7 +62,7 @@ export default function UserList() {
                             leftIcon={<Icon as={RiAddLine}
                             fontSize="20" />}
                             >
-                                Criar novo
+                                {isWideVersion && <Text>Criar novo</Text>}
                             </Button>
                         </Link>                     
                     </Flex>
@@ -79,36 +92,40 @@ export default function UserList() {
                                     </Tr>
                                 </Thead>
                                 <Tbody>
-                                    <Tr>
-                                        <Td paddingX={["4", "4", "6"]}>
-                                            <Checkbox colorScheme="pink" />
-                                        </Td>
-                                        <Td>
-                                            <Box>
-                                                <Text fontWeight="bold">
-                                                    Usuário1
-                                                </Text>
-                                                <Text fontSize="sm" color="gray.300">
-                                                    usuario@email.com
-                                                </Text>
-                                            </Box>
-                                        </Td>
-                                        { isWideVersion && <Td>
-                                            01 de Janeiro de 2023
-                                        </Td>}
-                                        { isWideVersion && <Td>
-                                        <Button
-                                            as="a"
-                                            size="sm"
-                                            fontSize="sm"
-                                            colorScheme="purple"
-                                            leftIcon={<Icon as={RiPencilLine}
-                                            fontSize="16" />}
-                                            >
-                                                Criar novo
-                                            </Button>
-                                        </Td>}
-                                    </Tr>
+                                    {data.map(user => {
+                                        return (
+                                            <Tr key={user.id}>
+                                                <Td paddingX={["4", "4", "6"]}>
+                                                    <Checkbox colorScheme="pink" />
+                                                </Td>
+                                                <Td>
+                                                    <Box>
+                                                        <Text fontWeight="bold">
+                                                            {user.name}
+                                                        </Text>
+                                                        <Text fontSize="sm" color="gray.300">
+                                                            {user.email}
+                                                        </Text>
+                                                    </Box>
+                                                </Td>
+                                                { isWideVersion && <Td>
+                                                    {user.createdAt}
+                                                </Td>}
+                                                { isWideVersion && <Td>
+                                                <Button
+                                                    as="a"
+                                                    size="sm"
+                                                    fontSize="sm"
+                                                    colorScheme="purple"
+                                                    leftIcon={<Icon as={RiPencilLine}
+                                                    fontSize="16" />}
+                                                    >
+                                                        Editar
+                                                    </Button>
+                                                </Td>}
+                                            </Tr>
+                                        )
+                                    })}
                                 </Tbody>
                             </Table>
                             <Pagination />
